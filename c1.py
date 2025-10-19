1a9d422d168c7bbf52565ba886fe50dd6dcf9d72
Adhritcoolentrepenaur <adhritrana5@gmail.com>
2025-10-19 12:15:27 +0530

    remove

--- c1.py
+++ c1.py
@@ -1,3 +1,7 @@
-f2=open('codingal.txt','r')
-print(f2.readlines())
-f2.close()
+with open('codingal.txt','r')as f2:
+    data = f2.readlines()
+    for line in data:
+        word = line.split()
+        print(word)
+
+    f2.close()
---
+++ c2.py
@@ -0,0 +1,8 @@
+import os
+if os.path.exists("Robotics.txt"):
+    print("file exists!!!")
+else:
+    print("This file doesn't exist!")
+#os.remove("Robotics.txt")
+import os
+os.rmdir("Removing Sample")
